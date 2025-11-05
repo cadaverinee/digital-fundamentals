4a049f3fb4bc862bd517e98d13f63159e8e58921
cadaverinee <nika.felis@yandex.ru>
2025-11-05 16:31:35 +0300

    Update analyser2.sh

--- scripts/2/analyser2.sh
+++ scripts/2/analyser2.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+for file in $(ls $1)
+do
+    echo -e "$1$file:"
+    for info in $(cat $1$file)
+    do
+        name=$(echo $info | cut -d ":" -f1 )
+        age=$(echo $info | cut -d ":" -f2)
+        email=$(echo $info | cut -d ":" -f3)
+        if [[ $email =~ .spbstu.ru$ ]]
+        then
+             echo -e "Имя:$name"
+             echo -e "Возраст:$age"
+             echo -e "Почта:$email"
+        fi
+    done
+done
